b76ca9a7a154d95c967c6c2ff37579c6096f23be
Daniel Iván López García <lopez.garcia1319@gmail.com>
2024-01-22 18:21:47 -0600

    cambios

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,8 +4,8 @@ import mysql from "mysql2"
 dotenv.config()
 
 export default mysql.createConnection({
-    host:process.env.HOST,
-    user:process.env.USER,
-    password:process.env.PASSWORD,
-    database:process.env.DB
+    host:"localhost",
+    user:"root",
+    password:"daniel.lopez",
+    database:"testdb"
 })
--- src/repositories/user.respository.ts
+++ src/repositories/user.respository.ts
@@ -1,12 +1,12 @@
 import connection from "../config/db";
-import { ResultSetHeader } from "mysql2";
+import { ResultSetHeader   } from "mysql2";
 import { User } from "../interfaces/user.interface";
 import { v4 as random } from "uuid";
 import bcrypt from "bcryptjs";
-import { json } from "stream/consumers";
+
 
 interface IUserRepository {
-  save(user: User): Promise<User | string>;
+  save(user: User):Promise<User|string>;
   retrieveAll(searchParam: {
     email: string;
     username: string;
@@ -18,44 +18,23 @@ interface IUserRepository {
 }
 
 class UserRepository implements IUserRepository {
-  async save(user: User): Promise<User> {
+  async save(user: User):Promise<User|string>{
 
     try {
-        let uuid = random();
-        let salt = await bcrypt.genSalt(10);
-        let hashPassword = await bcrypt.hash(user.password, salt);
-    
-        const checkUser = await this.retrieveAll({
-          email: user?.email,
-          username: user?.username,
-        });
-      if (
-        checkUser[0].email == user.email ||
-        checkUser[0].username == user.username
-      ) {
-        console.log("usuario ya esta registrado", checkUser[0].email);
-        return checkUser[0];
-      }
-      return new Promise((resolve, reject) => {
-        connection.query<ResultSetHeader>(
-          "INSERT INTO users (uuid,username,email,password) VALUES(?,?,?,?)",
-          [uuid, user.username, user.email, hashPassword],
-          (err, res) => {
-            if (err) {
-              console.log(err);
-              reject(err);
-            }
-
-            this.retrieveById(res.insertId)
-              .then((usuario) => resolve(usuario!))
-              .catch((e) => console.log(e));
-          }
-        );
-      });
+      let uuid = random();
+      let salt = await bcrypt.genSalt(10);
+      let hashPassword = await bcrypt.hash(user.password, salt);
+      let message:string=""
+      const usuaui = connection.execute(
+        "INSERT INTO users (uuid,username,email,password) VALUES(?,?,?,?)",
+        [uuid, user.username, user.email, hashPassword]);
+        console.log([usuaui].map(u=>u.))
+        return message
+        
     } catch (error) {
-      console.log(error);
-      return user
+      return "error en la consulta"
     }
+
   }
 
   retrieveById(id: number): Promise<User> {
